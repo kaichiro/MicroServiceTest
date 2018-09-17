be4bbdd855e78add002cf055c707a452b030ec28
Kaichiro <kcfg23@gmail.com>
2018-09-17 15:46:54 -0300

    . Atualizando projeto. .

---
+++ MicroServiceTest/Models/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServiceTest.Models
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+        }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+
+        // Additional code ...
+
+    }
+}
